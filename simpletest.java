e16cb6fd23719b849996397445a394c12b942264
Wender Cris de Souza Firmino <38754001+wender-firmino-aurea@users.noreply.github.com>
2018-05-24 00:54:37 -0300

    Update simpletest.java

--- simpletest.java
+++ simpletest.java
@@ -2,5 +2,6 @@ public class simpletest {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Hello, XO!");
     }
 }
